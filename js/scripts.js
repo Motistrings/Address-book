4144d3fd65ce3e4a3f36356e78036d7c3eb3469d
Motistrings <timgrand4929@gmail.com>
2020-04-15 13:38:45 +0300

    Added constructor

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,16 @@ function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
 }
-var tito = new Contact("Anthony", "Brown");
+$(document).ready(function () {
+  $("form#new-contact").submit(function (event) {
+    event.preventDefault();
+    var inputedFirstName = $("input#new-first-name").val();
+    var inputedLastName = $("input#new-last-name").val();
+    var newContact = new Contact(inputedFirstName, inputedLastName);
+    $("ul#contacts").append(
+      "<li><span class='contact'>" + newContact.firstName + "</span></li>"
+    );
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+  });
+});
